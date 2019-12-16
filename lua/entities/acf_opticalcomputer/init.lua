afac2b1ef78d6299593a10bae8f74112af0d8d02
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2019-12-16 18:45:13 -0300

    Updated optical computers
    Added HitPos vector output, will return { 0, 0, 0 } if not active
    Added Filter argument to ENT:GetTrace function for future use

--- lua/entities/acf_opticalcomputer/init.lua
+++ lua/entities/acf_opticalcomputer/init.lua
@@ -2,6 +2,8 @@
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
+-------------------------------[[ Local Functions ]]-------------------------------
+
 local function CheckLegal(Entity)
 	if Entity.Disabled then return end
 
@@ -23,6 +25,25 @@ local function CheckLegal(Entity)
 	end
 end
 
+local function ResetOutputs(Entity)
+	Wire_TriggerOutput(Entity, "Distance", 0)
+	Wire_TriggerOutput(Entity, "HitPos", { 0, 0, 0 })
+end
+
+local function GetTrace(Entity, Filter)
+	local TraceFilter = { Entity }
+
+	if Filter and next(Filter) then
+		for K, V in ipairs(Filter) do
+			TraceFilter[K + 1] = V
+		end
+	end
+
+	return util.QuickTrace(Entity:GetPos(), Entity:GetForward() * 50000, TraceFilter)
+end
+
+-------------------------------[[ Global Functions ]]-------------------------------
+
 function ENT:Initialize()
 	self:SetModel("models/props_lab/monitor01b.mdl")
 	self:SetMoveType(MOVETYPE_VPHYSICS)
@@ -30,7 +51,7 @@ function ENT:Initialize()
 
 	self.LegalMass = 65
 	self.Inputs = WireLib.CreateInputs(self, { "Active" })
-	self.Outputs = WireLib.CreateOutputs(self, { "Entity [ENTITY]", "Distance" })
+	self.Outputs = WireLib.CreateOutputs(self, { "Entity [ENTITY]", "Distance", "HitPos [VECTOR]" })
 
 	Wire_TriggerOutput(self, "Entity", self)
 
@@ -47,16 +68,18 @@ function ENT:TriggerInput(_, Value)
 	self.Active = tobool(Value)
 
 	if not self.Active then
-		Wire_TriggerOutput(self, "Distance", 0)
+		ResetOutputs(self)
 	end
 end
 
 function ENT:Think()
 	if not self.Disabled and self.Active then
-		local Trace = self:GetTrace()
-		local Distance = Trace.StartPos:Distance(Trace.HitPos)
+		local Trace = GetTrace(self)
+		local HitPos = Trace.HitPos
+		local Distance = Trace.StartPos:Distance(HitPos)
 
 		Wire_TriggerOutput(self, "Distance", Distance)
+		Wire_TriggerOutput(self, "HitPos", { HitPos[1], HitPos[2], HitPos[3] })
 	end
 
 	self:NextThink(CurTime() + 0.15)
@@ -73,7 +96,7 @@ end
 function ENT:Disable()
 	self.Disabled = true
 
-	Wire_TriggerOutput(self, "Distance", 0)
+	ResetOutputs(self)
 
 	-- ACF.IllegalDisableTime hasn't been implemented yet
 	timer.Simple(ACF.IllegalDisableTime or 30, function()
@@ -83,6 +106,4 @@ function ENT:Disable()
 	end)
 end
 
-function ENT:GetTrace()
-	return util.QuickTrace(self:GetPos(), self:GetForward() * 50000, self)
-end
+ENT.GetTrace = GetTrace
