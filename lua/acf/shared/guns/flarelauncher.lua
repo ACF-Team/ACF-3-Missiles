3b2472be9a6a5da5f0482117115f44be718262b0
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-02-21 06:04:21 -0300

    Used new weapon class functions on flare launchers

--- lua/acf/shared/guns/flarelauncher.lua
+++ lua/acf/shared/guns/flarelauncher.lua
@@ -28,3 +28,31 @@ ACF_defineGun("40mmFGL", { --id
 		propweight = 0.007
 	}
 })
+
+ACF.RegisterWeaponClass("FGL", {
+	Name		  = "Flare Launcher",
+	Description	  = "Flare Launchers can fire flares much more rapidly than other launchers, but can't load any other ammo types.",
+	MuzzleFlash	  = "gl_muzzleflash_noscale",
+	ROFMod		  = 0.6,
+	Spread		  = 1.5,
+	Sound		  = "acf_missiles/missiles/flare_launch.mp3",
+	Caliber	= {
+		Min = 25,
+		Max = 40,
+	},
+})
+
+ACF.RegisterWeapon("40mmGL", "FGL", {
+	Name		= "40mm Flare Launcher",
+	Description	= "Put on an all-American fireworks show with this flare launcher: high fire rate, low distraction rate. Fill the air with flare. Careful of your reload time.",
+	Model		= "models/missiles/blackjellypod.mdl",
+	Caliber		= 40,
+	Mass		= 75,
+	Year		= 1970,
+	MagSize		= 30,
+	MagReload	= 20,
+	Round = {
+		MaxLength = 9,
+		PropMass  = 0.007,
+	}
+})
