dff7ff4af62b9260c09d38f53b5bae000f71f126
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-11-19 01:24:06 -0300

    Updated missiles menu
    - Missiles menu now uses the ammo menu creation and update functions.

--- lua/acf/client/menu_items/missiles_menu.lua
+++ lua/acf/client/menu_items/missiles_menu.lua
@@ -1,74 +1,22 @@
-local AmmoTypes = ACF.Classes.AmmoTypes
+local ACF      = ACF
 local Missiles = ACF.Classes.Missiles
-local Crates = ACF.Classes.Crates
-local Racks = ACF.Classes.Racks
-local AmmoLists = {}
-local RackLists = {}
-local Selected = {}
-local Sorted = {}
-
-local function GetAmmoList(Class)
-	if not Class then return {} end
-	if AmmoLists[Class] then return AmmoLists[Class] end
-
-	local Result = {}
-
-	for K, V in pairs(AmmoTypes) do
-		if V.Unlistable then continue end
-		if V.Blacklist[Class] then continue end
-
-		Result[K] = V
-	end
-
-	AmmoLists[Class] = Result
-
-	return Result
-end
+local Racks    = ACF.Classes.Racks
+local Settings = { SuppressTracer = true }
 
 local function GetRackList(Data)
-	if not Data then return {} end
-	if RackLists[Data] then return RackLists[Data] end
-
 	local Result = {}
 
-	for Rack in pairs(Data.Racks) do
-		local Info = Racks[Rack]
+	if Data then
+		for Rack in pairs(Data.Racks) do
+			local Info = Racks[Rack]
 
-		if Racks[Rack] then
-			Result[Rack] = Info
+			if Racks[Rack] then
+				Result[Rack] = Info
+			end
 		end
 	end
-
-	RackLists[Data] = Result
 
 	return Result
-end
-
-local function LoadSortedList(Panel, List, Member)
-	local Choices = Sorted[List]
-
-	if not Choices then
-		Choices = {}
-
-		local Count = 0
-		for _, V in pairs(List) do
-			Count = Count + 1
-			Choices[Count] = V
-		end
-
-		table.SortByMember(Choices, Member, true)
-
-		Sorted[List] = Choices
-		Selected[Choices] = 1
-	end
-
-	Panel:Clear()
-
-	for _, V in pairs(Choices) do
-		Panel:AddChoice(V.Name, V)
-	end
-
-	Panel:ChooseOptionID(Selected[Choices])
 end
 
 local BaseText = "Caliber : %s\nMass : %s kg"
@@ -127,14 +75,7 @@ local function CreateMenu(Menu)
 	local RackPreview = RackBase:AddModelPreview()
 	local RackInfo = RackBase:AddLabel()
 
-	Menu:AddTitle("Ammo Settings")
-
-	local CrateList = Menu:AddComboBox()
-	local AmmoList = Menu:AddComboBox()
-
-	local AmmoBase = Menu:AddCollapsible("Ammo Information")
-	local AmmoDesc = AmmoBase:AddLabel()
-	local AmmoPreview = AmmoBase:AddModelPreview()
+	local AmmoList = ACF.CreateAmmoMenu(Menu, Settings)
 
 	ACF.WriteValue("PrimaryClass", "acf_rack")
 	ACF.WriteValue("SecondaryClass", "acf_ammo")
@@ -144,33 +85,30 @@ local function CreateMenu(Menu)
 	function MissileTypes:OnSelect(Index, _, Data)
 		if self.Selected == Data then return end
 
+		self.ListData.Index = Index
 		self.Selected = Data
-
-		local Choices = Sorted[Missiles]
-		Selected[Choices] = Index
 
 		ACF.WriteValue("WeaponClass", Data.ID)
 
 		MissileClass:SetText(Data.Description)
 
-		LoadSortedList(MissileList, Data.Items, "Caliber")
-		LoadSortedList(AmmoList, GetAmmoList(Data.ID), "Name")
+		ACF.LoadSortedList(MissileList, Data.Items, "Caliber")
+
+		AmmoList:LoadEntries(Data.ID)
 	end
 
 	function MissileList:OnSelect(Index, _, Data)
 		if self.Selected == Data then return end
 
+		self.ListData.Index = Index
 		self.Selected = Data
 
 		local Preview = Data.Preview
-		local ClassData = MissileTypes.Selected
-		local Choices = Sorted[ClassData.Items]
-		Selected[Choices] = Index
 
 		ACF.WriteValue("Weapon", Data.ID)
 		ACF.WriteValue("Destiny", Data.Destiny or "Missiles")
 
-		LoadSortedList(RackList, GetRackList(Data), "MagSize")
+		ACF.LoadSortedList(RackList, GetRackList(Data), "MagSize")
 
 		MissileDesc:SetText(Data.Description)
 
@@ -182,17 +120,16 @@ local function CreateMenu(Menu)
 
 		MissileInfo:SetText(GetMissileText(Data))
 
-		AmmoList:UpdateMenu()
+		ACF.UpdateAmmoMenu(Menu, Settings)
 	end
 
 	function RackList:OnSelect(Index, _, Data)
 		if self.Selected == Data then return end
 
+		self.ListData.Index = Index
 		self.Selected = Data
 
 		local Preview = Data.Preview
-		local Choices = Sorted[GetRackList(MissileList.Selected)]
-		Selected[Choices] = Index
 
 		ACF.WriteValue("Rack", Data.ID)
 
@@ -206,109 +143,7 @@ local function CreateMenu(Menu)
 		RackPreview:SetFOV(Preview and Preview.FOV or 75)
 	end
 
-	function CrateList:OnSelect(Index, _, Data)
-		if self.Selected == Data then return end
-
-		self.Selected = Data
-
-		local Preview = Data.Preview
-		local Choices = Sorted[Crates]
-		Selected[Choices] = Index
-
-		ACF.WriteValue("Crate", Data.ID)
-
-		AmmoPreview:SetModel(Data.Model)
-		AmmoPreview:SetCamPos(Preview and Preview.Offset or Vector(45, 60, 45))
-		AmmoPreview:SetLookAt(Preview and Preview.Position or Vector())
-		AmmoPreview:SetHeight(Preview and Preview.Height or 80)
-		AmmoPreview:SetFOV(Preview and Preview.FOV or 75)
-	end
-
-	function AmmoList:OnSelect(Index, _, Data)
-		if self.Selected == Data then return end
-
-		self.Selected = Data
-
-		local Choices = Sorted[GetAmmoList(MissileTypes.Selected.ID)]
-		Selected[Choices] = Index
-
-		ACF.WriteValue("AmmoType", Data.ID)
-
-		AmmoDesc:SetText(Data.Description .. "\n\nThis entity can be fully parented.")
-
-		self:UpdateMenu()
-	end
-
-	function AmmoList:UpdateMenu()
-		if not self.Selected then return end
-
-		local Ammo = self.Selected
-		local ToolData = ACF.GetToolData()
-
-		AmmoData = Ammo:ClientConvert(ToolData)
-
-		Menu:ClearTemporal(AmmoBase)
-		Menu:StartTemporal(AmmoBase)
-
-		if not Ammo.SupressDefaultMenu then
-			local RoundLength = AmmoBase:AddLabel()
-			RoundLength:TrackDataVar("Projectile", "SetText")
-			RoundLength:TrackDataVar("Propellant")
-			RoundLength:TrackDataVar("Tracer")
-			RoundLength:SetValueFunction(function()
-				local Text = "Round Length: %s / %s cm"
-				local CurLength = AmmoData.ProjLength + AmmoData.PropLength + AmmoData.Tracer
-				local MaxLength = AmmoData.MaxRoundLength
-
-				return Text:format(CurLength, MaxLength)
-			end)
-
-			local Projectile = AmmoBase:AddSlider("Projectile Length", 0, AmmoData.MaxRoundLength, 2)
-			Projectile:SetDataVar("Projectile", "OnValueChanged")
-			Projectile:SetValueFunction(function(Panel, IsTracked)
-				ToolData.Projectile = ACF.ReadNumber("Projectile")
-
-				if not IsTracked then
-					AmmoData.Priority = "Projectile"
-				end
-
-				Ammo:UpdateRoundData(ToolData, AmmoData)
-
-				ACF.WriteValue("Propellant", AmmoData.PropLength)
-
-				Panel:SetValue(AmmoData.ProjLength)
-
-				return AmmoData.ProjLength
-			end)
-
-			local Propellant = AmmoBase:AddSlider("Propellant Length", 0, AmmoData.MaxRoundLength, 2)
-			Propellant:SetDataVar("Propellant", "OnValueChanged")
-			Propellant:SetValueFunction(function(Panel, IsTracked)
-				ToolData.Propellant = ACF.ReadNumber("Propellant")
-
-				if not IsTracked then
-					AmmoData.Priority = "Propellant"
-				end
-
-				Ammo:UpdateRoundData(ToolData, AmmoData)
-
-				ACF.WriteValue("Projectile", AmmoData.ProjLength)
-
-				Panel:SetValue(AmmoData.PropLength)
-
-				return AmmoData.PropLength
-			end)
-		end
-
-		if Ammo.MenuAction then
-			Ammo:MenuAction(AmmoBase, ToolData, AmmoData)
-		end
-
-		Menu:EndTemporal(AmmoBase)
-	end
-
-	LoadSortedList(MissileTypes, Missiles, "ID")
-	LoadSortedList(CrateList, Crates, "ID")
+	ACF.LoadSortedList(MissileTypes, Missiles, "ID")
 end
 
 ACF.AddMenuItem(101, "Entities", "Missiles", "wand", CreateMenu)
