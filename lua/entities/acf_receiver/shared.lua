8509a4feb4a677fdb5c4a97cc9b11eeac644920d
LiddulBOFH <13317534+LiddulBOFH@users.noreply.github.com>
2023-08-10 11:55:53 -0500

    Fastforward dev to master, add warning receivers
    *Fastforward dev to master
    
    - Added radar/laser warning receivers
    These are capable of both telling that they are detected by their respective means, as well as provide a vague direction to the source
    
    -Laser warning receiver requires line of sight to the source, and needs the laser source to be pointed nearly on the unit before it returns, but it gives a rather accurate direction to it (nearest 2.5 degrees). This is also set off by the Wiremod laserpointer.
    
    -Radar warning receiver has the same requirements as a radar, and gives only a more vague direction (nearest 30 degrees) to the source. This is also set off by ACTIVE radar missiles, using their seek cone. This is NOT set off by missile-detecting radars, only targeting radars.
    
    Both of these receivers update on a rather slow basis compared to other components, at a whopping 500ms.

---
+++ lua/entities/acf_receiver/shared.lua
@@ -0,0 +1,9 @@
+DEFINE_BASECLASS("acf_base_simple")
+
+ENT.PrintName     = "ACF Receiver"
+ENT.Author        = "LiddulBOFH"
+ENT.WireDebugName = "ACF Receiver"
+ENT.PluralName    = "ACF Receivers"
+ENT.IsACFRadar       = true
+
+cleanup.Register("acf_receiver")
