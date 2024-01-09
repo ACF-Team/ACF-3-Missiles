ebe8891bf6baf2fd1e37f53a671a554cdc76d7c1
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2024-01-08 20:19:41 -0500

    Sound Extension Project hook documentation
    This hook should probably just be removed entirely tbh

--- lua/acf/hooks/hooks_missiles_sv.lua
+++ lua/acf/hooks/hooks_missiles_sv.lua
@@ -14,4 +14,11 @@ Hooks.Add("ACF_Missiles_Server", function(Gamemode)
 	function Gamemode:ACF_MissileCanExplode()
 		return true
 	end
+
+	--- Called when a missile plays its sound if the ACF Sound Extension Project by looterz is installed.
+	-- This is a legacy hook from ACF-2 and may be removed at any time.
+	-- @param Entity The missile entity to play sound on.
+	-- @param Sound The sound to play.
+	function Gamemode:ACF_SOUND_MISSILE()
+	end
 end)
