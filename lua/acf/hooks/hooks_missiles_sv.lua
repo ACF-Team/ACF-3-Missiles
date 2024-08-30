15251bb37d29b721d01eaee6913cf609cdb15b1f
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2024-08-30 17:14:35 -0400

    Standardize hook docs with function docs

--- lua/acf/hooks/hooks_missiles_sv.lua
+++ lua/acf/hooks/hooks_missiles_sv.lua
@@ -3,22 +3,22 @@ local Hooks = ACF.Utilities.Hooks
 
 Hooks.Add("ACF_Missiles_Server", function(Gamemode)
 	--- Called after a missile is fired.
-	-- @param Entity The missile entity that was launched.
+	--- @param Entity entity The missile entity that was launched.
 	function Gamemode:ACF_OnMissileLaunched()
 	end
 
 	--- Called when a missile attempts to create an explosion.
-	-- @param Entity The affected missile.
-	-- @param Data The bullet data of the affected missile.
-	-- @return True if the missile can explode, false otherwise.
+	--- @param Entity entity The affected missile.
+	--- @param Data table The bullet data of the affected missile.
+	--- @return boolean # True if the missile can explode, false otherwise.
 	function Gamemode:ACF_MissileCanExplode()
 		return true
 	end
 
 	--- Called when a missile plays its sound if the ACF Sound Extension Project by looterz is installed.
-	-- This is a legacy hook from ACF-2 and may be removed at any time.
-	-- @param Entity The missile entity to play sound on.
-	-- @param Sound The sound to play.
+	--- This is a legacy hook from ACF-2 and may be removed at any time.
+	--- @param Entity entity The missile entity to play sound on.
+	--- @param Sound string The sound to play.
 	function Gamemode:ACF_SOUND_MISSILE()
 	end
 end)
