1740b727beead5d0d5e0ffe3db64ffcd375a8b26
LengthenedGradient <leningrad.knight@gmail.com>
2025-01-29 19:36:01 -0500

    Update init.lua

--- lua/entities/acf_rack/init.lua
+++ lua/entities/acf_rack/init.lua
@@ -548,7 +548,7 @@ do -- Firing -----------------------------------
 		local Ang = Rack:LocalToWorldAngles(Point.Angle)
 		local Cone = math.tan(math.rad(Rack:GetSpread()))
 		local RandDir = (Rack:GetUp() * math.Rand(-1, 1) + Rack:GetRight() * math.Rand(-1, 1)):GetNormalized()
-		local Spread = Cone * RandDir * (math.random() ^ (1 / ACF.GunInaccuracyBias)) / (Rack.AccuracyCrewMod or 1)
+		local Spread = Cone * RandDir * (math.random() ^ (1 / ACF.GunInaccuracyBias))
 		local ShootDir = (Ang:Forward() + Spread):GetNormalized()
 
 		local Missile = Point.Missile
@@ -573,7 +573,7 @@ do -- Firing -----------------------------------
 	end
 
 	function ENT:GetSpread()
-		return self.Spread * ACF.GunInaccuracyScale
+		return self.Spread * ACF.GunInaccuracyScale / (self.AccuracyCrewMod or 1)
 	end
 
 	function ENT:Shoot()
