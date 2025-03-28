3286d5f8aae94fe33b19e31acda9c1d61a0836c7
fnden <cpking226@gmail.com>
2025-03-28 15:45:07 -0400

    left over debug reload
    oops

--- lua/acf/entities/missiles/kem.lua
+++ lua/acf/entities/missiles/kem.lua
@@ -18,7 +18,7 @@ Missiles.RegisterItem("MGM-166 KEM", "KEM", {
 	Mass		= 80,
 	Diameter	= 16.2 * ACF.InchToMm,
 	Year		= 1990,
-	ReloadTime	= 1,
+	ReloadTime	= 30,
 	ExhaustPos  = Vector(-20),
 	Racks		= {["1x LOSAT"] = true, ["2x LOSAT"] = true, ["6x LOSAT"] = true},
 	Navigation  = "APN",
